2728cca5e77f9dad3f9c73d851296ed3e7757df6
NCMIT <ncmituepg2025@gmail.com>
2025-10-29 18:37:14 -0300

    fix/eyeofficon size

--- components/common/Icon.tsx
+++ components/common/Icon.tsx
@@ -126,7 +126,7 @@ export const EyeIcon: React.FC<IconProps> = (props) => (
 );
 
 export const EyeOffIcon: React.FC<IconProps> = (props) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" {...props}>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 23 23" stroke="currentColor" {...props}>
       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.542-7 1.274-4.057 5.064-7 9.542-7 .847 0 1.67.127 2.455.364m-6.04 7.118a3.004 3.004 0 014.243-4.242M3 3l18 18" />
     </svg>
 );
